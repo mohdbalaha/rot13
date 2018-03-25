5a0a5f797959f574653c3fcb7eec2b738a516c48
Mohamad Balaha <35835415+mohdbalaha@users.noreply.github.com>
2018-03-26 01:34:39 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rot13
-Simple letter substitution cipher that replaces a letter with the 13th letter after it.
+# Rot13
+ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher that replaces a letter with the 13th letter after it, in the alphabet. ROT13 is a special case of the Caesar cipher, developed in ancient Rome.
---
+++ script.js
@@ -0,0 +1,65 @@
+let btn = document.getElementById('cipher');
+btn.onclick = function () {
+    txt.value = rot13(txt.value);
+}
+
+// let btn2 = document.getElementById('decipher');
+// btn2.onclick = function () {
+//     txt.value = rot13(txt.value);
+// }
+
+let txt = document.getElementById('text');
+
+
+
+// let out = document.getElementById('output');
+
+function rot13(message) {
+    //your code here
+    let arr = message.split(' ');
+    let newArr = [];
+    for (let item of arr) {
+        newArr.push(cipher(item));
+    }
+    let newMessage = newArr.join(' ');
+    //function that ciphers a single word
+    function cipher(word) {
+        let char = '';
+        let ascii = 0;
+        let newWord = '';
+        for (let index in word) {
+            ascii = word.charCodeAt(index);
+
+            if (ascii >= 97 && ascii <= 122) {
+                //Small letter
+                if (ascii >= 97 && ascii <= 109) {
+                    ascii += 13;
+                    char = String.fromCharCode(ascii);
+                } else {
+                    ascii -= 13;
+                    char = String.fromCharCode(ascii);
+                }
+                newWord += char;
+
+            } else if (ascii >= 65 && ascii <= 90) {
+                //Capital letter
+                if (ascii >= 65 && ascii <= 77) {
+                    ascii += 13;
+                    char = String.fromCharCode(ascii);
+                } else {
+                    ascii -= 13;
+                    char = String.fromCharCode(ascii);
+                }
+                newWord += char;
+
+            } else {
+                //Not a letter
+                char = String.fromCharCode(ascii);
+                newWord += char;
+            }
+        }
+        return newWord;
+    }
+
+    return newMessage;
+}
